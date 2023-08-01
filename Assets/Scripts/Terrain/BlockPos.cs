ecfb84f2c23ee8873c1fdf3ed36a0641b2f33efa
MKKL1 <Mkkl3301@gmail.com>
2023-08-02 00:18:23 +0200

    Block collector for holding terrain changes

---
+++ Assets/Scripts/Terrain/BlockPos.cs
@@ -0,0 +1,21 @@
+﻿using Terrain.Blocks;
+using UnityEngine;
+
+namespace Terrain
+{
+    public readonly struct BlockPos
+    {
+        private readonly BlockBase blockBase;
+        private readonly Vector2Int pos;
+
+        public BlockPos(BlockBase blockBase, Vector2Int pos)
+        {
+            this.blockBase = blockBase;
+            this.pos = pos;
+        }
+
+        public BlockBase BlockBase => blockBase;
+
+        public Vector2Int Pos => pos;
+    }
+}
